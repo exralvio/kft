7b9f1fd1f4d9903c73516acc3c8b9d9874a54e62
Alvio Leegia <exralvio@gmail.com>
2018-03-08 22:22:01 +0700

    update uploader

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -4,7 +4,9 @@ $(function(){
 	var dropzoneOptions = {
 		dictDefaultMessage: false,
 		addRemoveLinks: true,
-		clickable: ".dz-addfile"
+		clickable: ".dz-addfile",
+		thumbnailWidth: 170,
+		thumbnailHeight: 110
 	};
 	
 	var mydropzone = new Dropzone('#uploadzone', dropzoneOptions);
@@ -12,11 +14,19 @@ $(function(){
 	mydropzone.on('addedfile', function(file){
 		$('.dz-message').hide();
 		$('.dz-addmore').show();
+		$('#uploadzone').removeClass('col-md-12').addClass('col-md-9');
+		$('.form-uploader-wrapper').show();
 	});
 
 	mydropzone.on('reset', function(){
 		$('.dz-addmore').hide();
 		$('.dz-message').show();
+		$('.form-uploader-wrapper').hide();
+		$('#uploadzone').removeClass('col-md-9').addClass('col-md-12');
+	});
+
+	mydropzone.on('complete', function(file){
+		console.log(file);
 	});
 
 	mydropzone.on('queuecomplete', function(){
