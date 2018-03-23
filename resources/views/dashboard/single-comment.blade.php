ff65c7c9a2e0b606b3fafd6d83e6c96515eca3b5
Fazrin Mutaqin <v-fazrin.mutaqin@btpn.com>
2018-03-23 17:43:05 +0700

    del button on single-comment

--- resources/views/dashboard/single-comment.blade.php
+++ resources/views/dashboard/single-comment.blade.php
@@ -5,5 +5,8 @@
     <div id="del-{{ $comment->_id }}" class="col-md-10 comment-input comment-text">
         <div style="font-weight: bold;">{{ $comment->user_detail['first_name'] }} {{ $comment->user_detail['last_name'] }}</div>
         {{ $comment->comment }}
+        @if(Session::get('user')->_id == $comment->user_id)
+        <div id="del-{{ $comment->_id }}" class="del-comment" style="width: 1px;height: 1px;float: right;cursor: pointer;display:none">x</div>
+        @endif
     </div>
 </div>
