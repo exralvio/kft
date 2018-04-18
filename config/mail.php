ef735fc057e8295b182c38c63675c8f12b9541e7
Alvio Leegia <exralvio@gmail.com>
2018-04-19 01:45:49 +0700

    update default email

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'mail@kft.id'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@kft.id'),
         'name' => env('MAIL_FROM_NAME', 'KFT'),
     ],
 
