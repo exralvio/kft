5a61bfc06675fe2e6dcf57e27c52b7ca4027b240
Fazrin Mutaqin <v-fazrin.mutaqin@btpn.com>
2018-04-19 19:50:34 +0700

    Admin Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -14,8 +14,9 @@
 
 <section class="content">
   <div class="row">
-    <div class="col-xs-12">
-      here is dashboard content
+    <div class="col-xs-12" style="text-align: center;margin-top: 10%;">
+      <img src="{{ url('images/logo-black.png') }}"/>
+      <h1>Welcome to KFT Admin Page</h1>
     </div>
     <!-- /.col -->
   </div>
