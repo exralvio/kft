a1f7a4809924b66e3d7f6d157e3ae92af1ef5f50
Fazrin Mutaqin <v-fazrin.mutaqin@btpn.com>
2018-05-23 11:10:48 +0700

    [Share][FB] add OG Meta on media detail

--- resources/views/media/detail.blade.php
+++ resources/views/media/detail.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts/global')
- 
+
+@section('header_script')
+<meta property="fb:app_id" content="1689701714455676" />
+<meta property="og:site_name" content="KFT"/>
+<meta property="og:url" content="{{ url('media').'/'.$post->_id }}"/>
+<meta property="og:type" content="article"/>
+<meta property="og:locale" content="id_ID"/>
+<meta property="og:title" content="{{ $post->title }}"/>
+<meta property="og:image" content="{{ url($post['images']['large']) }}"/>
+<meta property="og:description" content="{{ $post->description }}"/>
+@endsection
+
 @section('page-title')
  Komunitas Fotografi Telkom
 @endsection
