5ab90a4fd37b0fb96ce5f0b8ec318912b743abd8
Alvio Leegia <exralvio@gmail.com>
2018-06-14 11:11:09 +0700

    update login page

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,90 +1,84 @@
-@extends('layouts/landing')
- 
-@section('page-title')
- Login
-@endsection
-     
-@section('content')       
-    <style type="text/css">
-        .or-login-with{
-            width: 100%;
-            border-bottom: 1px solid #000;
-            line-height: 0.1em;
-            margin: 20px 0px;
-        }
-        .or-login-with span{
-            background: #f8f8f8;
-            padding: 0 10px;
-            font-size: 17px;
-            font-weight: bold;
-        }
-    </style>
-    <!-- Section -->
-    <section class="page-section pb-50">
-        <div class="container relative">
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="{{ url('') }}/admin_lte/components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ url('') }}/admin_lte/components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="{{ url('') }}/admin_lte/components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ url('') }}/admin_lte/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="{{ url('') }}/admin_lte/dist/css/custom.css">
 
-                <!-- Login Form -->                            
-                <div class="row">
-                    <div class="col-md-4 col-md-offset-4 bg-gray-lighter pb-40">
-                        <h1 class="align-center">Admin LogIn KFT</h1>
-                        <form class="form contact-form" id="contact_form" method="post" action="{{ url('admin/login') }}">
-                            {{ csrf_field() }}
-                            <div class="clearfix">
-                                
-                                <!-- email -->
-                                <div class="form-group">
-                                    <input type="text" name="email" id="email" class="input-md round form-control" placeholder="Email" pattern=".{3,100}" required>
-                                    @if ($errors->has('email'))
-                                        <span class="text-danger">{{ $errors->first('email') }}</span>
-                                    @endif
-                                </div>
-                                
-                                <!-- Password -->
-                                <div class="form-group">
-                                    <input type="password" name="password" id="password" class="input-md round form-control" placeholder="Password" pattern=".{5,100}" required>
-                                    @if ($errors->has('password'))
-                                        <span class="text-danger">{{ $errors->first('password') }}</span>
-                                    @endif
-                                </div>
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 
-                                <div class="form-group">
-                                    @if ($errors->has('failedLogin'))
-                                        <span class="text-danger">{{ $errors->first('failedLogin') }}</span>
-                                    @endif
-                                </div>
-                                    
-                            </div>
-                            
-                            <div class="clearfix">
-                                
-                                <div class="cf-left-col">
-                                    
-                                    <!-- Inform Tip -->                                        
-                                    <div class="form-tip pt-20">
-                                        <a href="{{url('recover-password')}}">Forgot Password?</a>
-                                    </div>
-                                    
-                                </div>
-                                
-                                <div class="cf-right-col">
-                                    
-                                    <!-- Send Button -->
-                                    <div class="align-right pt-10">
-                                        <button class="submit_btn btn btn-mod btn-medium btn-round" id="login-btn">Login</button>
-                                    </div>
-                                    
-                                </div>
-                                
-                            </div>
-                            
-                        </form>
-                        
-                    </div>
-                </div>
-                <!-- End Login Form -->
-                
-            
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="{{ url('') }}/admin_lte/index2.html"><b>Admin</b>KFT</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body pt-50 pb-40">
+    <form action="{{ url('admin/login') }}" method="post">
+        {{ csrf_field() }}
+      <div class="form-group has-feedback">
+        <input type="email" name="email" class="form-control" placeholder="Email">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        @if ($errors->has('email'))
+            <span class="text-danger">{{ $errors->first('email') }}</span>
+        @endif
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" name="password" class="form-control" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        @if ($errors->has('password'))
+            <span class="text-danger">{{ $errors->first('password') }}</span>
+        @endif
+      </div>
+
+      <div class="form-group">
+        @if ($errors->has('failedLogin'))
+            <span class="text-danger">{{ $errors->first('failedLogin') }}</span>
+        @endif
+    </div>
+
+      <div class="row">
+        <div class="col-xs-8">
+          
         </div>
-    </section>
-    <!-- End Section -->
-@endsection
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 3 -->
+<script src="{{ url('') }}/admin_lte/components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{{ url('') }}/admin_lte/components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="{{ url('') }}/admin_lte/plugins/iCheck/icheck.min.js"></script>
+
+</body>
+</html>
